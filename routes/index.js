71ba284b69947674f6e527e2362b51418c4faae1
Jahid <s.jahid.90@gmail.com>
2020-10-11 13:05:02 +0530

    add route to serve navigation header

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/header', (req, res, next) => {
+  res.render('header');
 });
 
 module.exports = router;
